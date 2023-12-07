85120a30d1fffed30714e69241dba21567b1e6e3
Matt <153225699+ScivenMatt@users.noreply.github.com>
2023-12-07 12:08:14 -0800

    Update and rename Arduino_Motors_Code.ino to Arduino_Motors_Complete_Code.ino

--- Arduino_Motors_Complete_Code.ino
+++ Arduino_Motors_Complete_Code.ino
@@ -1,25 +1,42 @@
 
 /*############## Pin Variables Here ##############*/
+const int PWM_Pin = 3;
+const int Button_Pin = 13;
 
+int count = 0;
 
 /*############## End Of Pin Variables ###############  */
 
 void setup() {
 
+  pinMode(Button_Pin, INPUT_PULLUP);
+  pinMode(PWM_Pin, OUTPUT);
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  
+  if(Button_Pressed(Button_Pin)){
+    count = count + 1;
+  }
+
+  if(count == 0){
+  analogWrite(PWM_Pin, 0); 
+  }
+  else if(count == 1){
+  analogWrite(PWM_Pin,50); 
+  }
+  else if(count == 2){
+  analogWrite(PWM_Pin,100); 
+  }
+  else if(count == 3){
+  analogWrite(PWM_Pin,200); 
+  }
+  else{
+  count = 0; 
+  }
+
 }
-
-
-
-
-
-
-
-
 
 
 
